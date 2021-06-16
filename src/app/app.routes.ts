05c542827b5f70bea214da48524ce61b551ea63e
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-16 22:11:12 +0700

    refactor: remove unused files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-import aboutRoutes from "./about/about.routes";
 import homeRoutes from "./home/home.routes";
 
-const routes: RouteRecordRaw[] = [...aboutRoutes, ...homeRoutes];
+const routes: RouteRecordRaw[] = [...homeRoutes];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
