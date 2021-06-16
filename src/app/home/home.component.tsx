91e2b4f94f80fcc7349892f469435241a55b71a3
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-16 22:09:29 +0700

    improvement: update home page UI

--- src/app/home/home.component.tsx
+++ src/app/home/home.component.tsx
@@ -1,15 +1,38 @@
 import { defineComponent } from "vue";
 
-import Logo from "@/app/shared/assets/images/logo.png";
-import HelloWorld from "@/app/shared/components/hello-world/hello-world.component.vue";
+import Camera from "@/app/shared/assets/images/camera.png";
+import Image from "@/app/shared/assets/images/image.png";
+import Payment from "@/app/shared/assets/images/payment.png";
 
 export default defineComponent({
   name: "Home",
 
   render: (): JSX.Element => (
-    <div class="home">
-      <img alt="Vue logo" src={Logo} />
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class=" bg-white flex flex-col h-screen justify-evenly px-6 select-none">
+      <div class="text-center">
+        <h1 class="font-semibold mb-2 text-3xl">Split Bill</h1>
+
+        <p class="font-light text-gray-500 text-sm">
+          Got a bill to split? Take a photo of the receipt and find out how much
+          everyone owes!
+        </p>
+      </div>
+
+      <div class="flex justify-center">
+        <img src={Payment} alt="Payment Bill Icon" />
+      </div>
+
+      <div class="flex">
+        <div class="cursor-pointer flex flex-col items-center p-4 w-full">
+          <img src={Camera} alt="Camera Icon" class="mb-2" width="32" />
+          <p class="text-sm">Take photo</p>
+        </div>
+
+        <div class="cursor-pointer flex flex-col items-center p-4 w-full">
+          <img src={Image} alt="Image Icon" class="mb-2" width="32" />
+          <p class="text-sm">Select photo</p>
+        </div>
+      </div>
     </div>
   ),
 });
